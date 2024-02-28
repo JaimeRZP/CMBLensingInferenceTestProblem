3bfe580ec539567b9ff492aced065759850e91b7
jaimerzp <jaimerz011235813@gmail.com>
2024-02-28 20:29:50 +0000

    upd

--- examples/MCHMC.jl
+++
@@ -1,8 +0,0 @@
-using Pkg
-Pkg.activate(".")
-Pkg.instantiate()
-
-using Revise, Adapt, CMBLensing, CMBLensingInferenceTestProblem, CUDA, JLD2, LaTeXStrings, LinearAlgebra, 
-    MCMCChains, MCMCDiagnosticTools, MicroCanonicalHMC, MuseInference, MuseInference, Plots, 
-    ProgressMeter, Random, Statistics, Zygote
-
